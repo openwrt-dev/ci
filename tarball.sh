5812c49dc0714fb65228cd7b134734d5f5a3839f
pexcn <i@pexcn.me>
2021-03-09 02:11:47 +0800

    update

--- tarball.sh
+++ tarball.sh
@@ -3,11 +3,7 @@
 copy_binaries() {
   local sub_path=$(echo ${CONFIG_URL##*/} | tr '-' '/')
   mkdir binaries
-  cp -v openwrt/bin/targets/$sub_path/*factory.bin binaries/ 2>/dev/null || true
-  cp -v openwrt/bin/targets/$sub_path/*sysupgrade.bin binaries/ 2>/dev/null || true
-  cp -v openwrt/bin/targets/$sub_path/*combined*.img.gz binaries/ 2>/dev/null || true
-  cp -v openwrt/bin/targets/$sub_path/*.buildinfo binaries/ 2>/dev/null || true
-  cp -v openwrt/bin/targets/$sub_path/sha256sums binaries/ 2>/dev/null || true
+  cp -v openwrt/bin/targets/$sub_path/* binaries/ 2>/dev/null || true
 }
 
 create_tarball() {
