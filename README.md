d98f145c3ecf54019d207fd67e2652cdc4255086
pexcn <pexcn97@gmail.com>
2023-10-21 12:29:52 +0800

    backport changes from openwrt-23.05

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # OpenWrt CI
 
-[![Build](https://github.com/openwrt-dev/ci/workflows/Build/badge.svg)](https://github.com/openwrt-dev/ci/actions)
+[![Build](https://github.com/openwrt-dev/ci/workflows/Build/badge.svg?branch=openwrt-19.07)](https://github.com/openwrt-dev/ci/actions)
--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
-#!/bin/bash -e
-set -o pipefail
-
-[ "$VERBOSE_LOG" == 1 ] && set -x
+#!/bin/sh
+# shellcheck disable=SC3043,SC2086,SC2164,SC2103,SC2046
 
 get_sources() {
-  git clone https://github.com/openwrt-dev/openwrt.git --single-branch -b ${GITHUB_REF##*/}
+  git clone $BUILD_REPO --single-branch -b $GITHUB_REF_NAME openwrt
+}
+
+echo_version() {
+  echo "[=============== openwrt version ===============]"
+  cd openwrt && git log -1 && cd -
+  echo
+  echo "[=============== configs version ===============]"
+  cd configs && git log -1 && cd -
 }
 
 build_firmware() {
@@ -13,11 +19,19 @@ build_firmware() {
   ./scripts/feeds update -a
   ./scripts/feeds install -a
 
-  cp ../configs/$BUILD_ARCH .config
-  [ "$VERBOSE_LOG" != 1 ] && make -j$(nproc) V=w || make -j1 V=sc
+  cp ${GITHUB_WORKSPACE}/configs/${BUILD_PROFILE} .config
+  make -j$(nproc) V=w || make -j1 V=sc || exit 1
 
-  cd ..
+  cd -
+}
+
+package_binaries() {
+  local bin_dir="openwrt/bin"
+  local tarball="${BUILD_PROFILE}.tar.gz"
+  tar -zcvf $tarball -C $bin_dir $(ls $bin_dir -1)
 }
 
 get_sources
+echo_version
 build_firmware
+package_binaries
