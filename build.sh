e7e7ce931d638d5c37f430b236db43592df5ae20
pexcn <pexcn97@gmail.com>
2023-11-19 12:17:24 +0800

    sync ci-ipq60xx changes

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ build_firmware() {
   ./scripts/feeds install -a
 
   cp ${GITHUB_WORKSPACE}/configs/${BUILD_PROFILE} .config
-  make -j$(nproc) V=w || make -j1 V=sc || exit 1
+  make -j$(($(nproc) + 1)) V=e || make -j1 V=sc || exit 1
 
   cd -
 }
@@ -31,7 +31,17 @@ package_binaries() {
   tar -zcvf $tarball -C $bin_dir $(ls $bin_dir -1)
 }
 
+package_dl_src() {
+  [ -n "$BACKUP_DL_SRC" ] || return 0
+  [ $BACKUP_DL_SRC = 1 ] || return 0
+
+  local dl_dir="openwrt/dl"
+  local tarball="${BUILD_PROFILE}_dl-src.tar.gz"
+  tar -zcvf $tarball -C $dl_dir $(ls $dl_dir -1)
+}
+
 get_sources
 echo_version
 build_firmware
 package_binaries
+package_dl_src
